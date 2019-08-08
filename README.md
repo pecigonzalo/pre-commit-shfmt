2b092dd4d2744bddd045c92b349459bdbdfa3d8f
Faustin Lammler <faustin@fala.red>
2019-08-08 17:31:59 +0200

    Simplify shfmt wrapper
    - based on https://github.com/detailyang/pre-commit-shell

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
 # pre-commit-shfmt hook
 
-Single [pre-commit](http://pre-commit.com/) hook which runs **[shfmt](https://github.com/mvdan/sh)** on shell script files.
-
+Single [pre-commit](http://pre-commit.com/) hook which runs
+**[shfmt](https://github.com/mvdan/sh)** on shell script files.
 
 An example `.pre-commit-config.yaml`:
 
 ```yaml
--   repo: git://github.com/pecigonzalo/pre-commit-fmt
-    sha: master
-    hooks:
-      -   id: shell-fmt
-          args:
-            - --indent=4
+- repo: https://github.com/fauust/pre-commit-fmt
+  sha: master
+  hooks:
+    -   id: shell-fmt
+        args: ['--indent=4']
 ```
 
 Enjoy the clean code!
--- shfmt.sh
+++ shfmt.sh
@@ -1,33 +1,15 @@
 #!/usr/bin/env bash
 
-set -e          # exit on command errors
-set -o nounset  # abort on unbound variable
-set -o pipefail # capture fail exit codes in piped commands
-# set -x          # execution tracing debug messages
+set -o errexit
+set -o pipefail
+set -o nounset
 
-indentation=""
+DEBUG=${DEBUG:=0}
+[[ $DEBUG -eq 1 ]] && set -o xtrace
 
-while [[ $# -gt 0 ]] && [[ ."$1" = .-*  || ."$1" = .--* ]]; do
-    # Get param and value using parameter expansion, splitting on = or " "
-    param="${1%[ =]*}"
-    value="${1#*[ =]}"
-    shift
-    case "$param" in
-        -i | --indent)
-            indentation="-i $value"
-            ;;
-        -*)
-            echo "Error: Unknown option: $param" >&2
-            exit 1
-            ;;
-        *)  # No more options
-            break 2
-            ;;
-    esac
-done
+if ! command which shfmt &>/dev/null; then
+  echo >&2 'shfmt command not found'
+  exit 1
+fi
 
-for file in "$@"; do
-    if file "$file" | grep -Pi 'shell script' > /dev/null; then
-        shfmt -l -w $indentation "$file"
-    fi
-done
+shfmt "$@"
